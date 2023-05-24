0679fd01bf15418e67d7ce326e0855820bb08227
Nick Liebmann <nliebmann@dhcp017104094139.corp.apple.com>
2023-05-24 15:41:56 -0500

    feat: nothing I

--- config.py
+++
@@ -1,5 +0,0 @@
-CUSTOM_RULES = {
-    "Full name": ["FIRST_NAME", " ", "LAST_NAME"],
-    "Signature": ["FIRST_NAME", " ", "MIDDLE_NAME", " ", "LAST_NAME"],
-    "Address": ["62470 Reje Ct.", " ", "USERS_CITY", ", ", "USERS_STATE", " ", "USERS_ZIP_CODE"]
-}
